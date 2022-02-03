e0c0ca39c324a90f34fc136bb49bb4d51e3f38ba
walker-cook <jwcook99@gmail.com>
2022-02-03 16:51:42 +0000

    DOne

--- app/controllers/currencies_controller.rb
+++ app/controllers/currencies_controller.rb
@@ -23,4 +23,20 @@ class CurrenciesController < ApplicationController
     render({:template => "currency_templates/step_two.html.erb"})
 
   end 
+
+  def third_currency
+    @from_symbol = params.fetch("from_currency")
+    @to_symbol = params.fetch("to_currency")
+
+    @raw_data = open("https://api.exchangerate.host/convert?from=" + @from_symbol + "&to=" + @to_symbol).read
+    @parsed_data = JSON.parse(@raw_data)
+    @info_hash = @parsed_data.fetch("info")
+    
+    #@array_of_symbols = @symbols_hash.keys
+
+    #@from_symbol = params.fetch("from_currency")
+
+    render({:template => "currency_templates/step_three.html.erb"})
+
+  end 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   get("/forex", {:controller => "currencies", :action => "first_currency"})
   get("/forex/:from_currency", {:controller => "currencies", :action => "second_currency"})
+  get("/forex/:from_currency/:to_currency", {:controller => "currencies", :action => "third_currency"})
 end
